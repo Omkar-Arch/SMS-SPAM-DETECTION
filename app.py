3881b7d60bdb307a42e4361d95393a584cc4fb03
omkar <omkarsrivastava786@gmail.com>
2026-06-10 11:33:42 +0530

    Fixed NLTK deployment issue

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ import pickle
 import nltk
 import string
 
+import nltk
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
